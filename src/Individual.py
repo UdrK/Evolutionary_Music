1095943a72896f28d65ff7c0b010ee4b5c6bec35
UdrK <nomefittizioqualunque@gmail.com>
2020-10-08 20:30:02 +0200

    removing dynamics, changing environment to log more information

--- src/Individual.py
+++ src/Individual.py
@@ -1,7 +1,6 @@
 """
 This class follows the approach:
 The genetic information is relative to the probabilities of picking each note from a predetermined scale
- ith predetermined root
 
 Basically it will be a Matrix m x n, with m the number of notes in the musical phrase and n the number
 of notes in the scale.
@@ -15,15 +14,15 @@ import random
 class IndividualBuilder:
 
     @staticmethod
-    def from_chance(root, scale, phrase_length):
+    def from_chance(root, scale, phrase_length, id):
         ind = Individual()
+        ind.id = id
         ind.root = root
         ind.scale = scale.copy()
         ind.phrase_length = phrase_length
         # 3 octaves of the scale
-        ind.possible_notes = [root+j+i for j in [-12, 0, 12] for i in scale]
+        ind.possible_notes = [root+j+i for j in [0] for i in scale]
         ind.possible_durations = [WN, DHN, HN, DQN, QN, DEN, EN, DSN, SN]
-        ind.possible_dynamic = [FF, F, MF, MP, P, PP]
 
         # initializing empty notes probabilities matrix
         ind.note_genes = [[0 for m in range(len(ind.possible_notes))] for m in range(phrase_length)]
@@ -54,27 +53,12 @@ class IndividualBuilder:
             for j in range(len(ind.possible_durations)):
                 ind.duration_genes[i][j] = random_probabilities[j]
 
-
-        # initializing empty dynamics probabilities matrix
-        ind.dynamic_genes = [[0 for m in range(len(ind.possible_dynamic))] for m in range(phrase_length)]
-        # initializing random dynamics probabilities matrix
-        for i in range(phrase_length):
-
-            # n = len(possible_notes) random numbers such that their sum is == 1
-            # generate n random numbers, divide each by their sum
-            random_probabilities = [random.randint(0, 10000) for m in range(len(ind.possible_dynamic))]
-            total = sum(random_probabilities)
-            random_probabilities = [m/total for m in random_probabilities]
-
-            for j in range(len(ind.possible_dynamic)):
-                ind.dynamic_genes[i][j] = random_probabilities[j]
-
         return ind
 
     @staticmethod
-    def from_individuals(individual1, individual2):
+    def from_individuals(individual1, individual2, id):
 
-        if not individual1.root  == individual2.root:
+        if not individual1.root == individual2.root:
             return None
         if not individual1.scale == individual2.scale:
             return None
@@ -82,16 +66,16 @@ class IndividualBuilder:
             return None
 
         ind = Individual()
+        ind.id = id
+        ind.parents = [individual1.id, individual2.id]
         ind.root = individual1.root
         ind.scale = individual1.scale.copy()
         ind.phrase_length = individual1.phrase_length
         ind.possible_notes = individual1.possible_notes.copy()
         ind.possible_durations = individual1.possible_durations.copy()
-        ind.possible_dynamic = individual1.possible_dynamic.copy()
 
         ind.note_genes = []
         ind.duration_genes = []
-        ind.dynamic_genes = []
 
         for i in range(len(individual1.note_genes)):
             ind.note_genes.append(Individual._sum_genes(individual1.note_genes[i], individual2.note_genes[i]))
@@ -99,8 +83,6 @@ class IndividualBuilder:
         for i in range(len(individual1.duration_genes)):
             ind.duration_genes.append(Individual._sum_genes(individual1.duration_genes[i], individual2.duration_genes[i]))
 
-        for i in range(len(individual1.dynamic_genes)):
-            ind.dynamic_genes.append(Individual._sum_genes(individual1.dynamic_genes[i], individual2.dynamic_genes[i]))
 
         return ind
 
@@ -115,15 +97,15 @@ class Individual:
             phrase_length: int represents how many notes should be in the phrase
     """
     def __init__(self):
+        self.id = None
+        self.parents = None
         self.root = None
         self.scale = None
         self.phrase_length = None
         self.possible_notes = None
         self.possible_durations = None
-        self.possible_dynamic = None
         self.note_genes = None
         self.duration_genes = None
-        self.dynamic_genes = None
 
     """
         returns individual generated following its genes
@@ -131,18 +113,15 @@ class Individual:
     def generate(self):
         notes = []
         durations = []
-        dynamics = []
 
         for i in range(self.phrase_length):
             pitch = random.choices(self.possible_notes, self.note_genes[i])
             duration = random.choices(self.possible_durations, self.duration_genes[i])
-            dynamic = random.choices(self.possible_dynamic, self.dynamic_genes[i])
 
             notes.append(pitch[0])
             durations.append(duration[0])
-            dynamics.append(dynamic[0])
 
-        return notes, durations, dynamics
+        return notes, durations
 
     """
         saves genes in .txt file
@@ -153,14 +132,11 @@ class Individual:
 
         note_matrix_string = Individual._matrix_to_string(self.note_genes)
         duration_matrix_string = Individual._matrix_to_string(self.duration_genes)
-        dynamic_matrix_string = Individual._matrix_to_string(self.dynamic_genes)
 
         with open(filename, "w") as output_file:
             output_file.write(note_matrix_string)
             output_file.write("\n")
             output_file.write(duration_matrix_string)
-            output_file.write("\n")
-            output_file.write(dynamic_matrix_string)
 
     """
         reads genes saved in .txt file
@@ -171,11 +147,9 @@ class Individual:
         with open(filename, "r") as input_file:
             note_matrix_string = input_file.readline()
             duration_matrix_string = input_file.readline()
-            dynamic_matrix_string = input_file.readline()
 
         self.note_genes = Individual._string_to_matrix(note_matrix_string)
         self.duration_genes = Individual._string_to_matrix(duration_matrix_string)
-        self.dynamic_genes = Individual._string_to_matrix(dynamic_matrix_string)
 
     """
         takes 2 gene arrays and returns a new array with higher probabilities where both given arrays have higher 
@@ -225,6 +199,6 @@ class Individual:
         for i in range(len(rows)):
             numbers = rows[i].split(',')
             for j in range(len(numbers)):
-                matrix[i][j] = numbers[j]
+                matrix[i][j] = float(numbers[j])
 
         return matrix
--- src/visualize_genes.py
+++ src/visualize_genes.py
@@ -1,23 +1,28 @@
+"""
+This module contains functions to visualize random_individual's genes
+"""
+
 import matplotlib.pyplot as plt
-from individual import Individual, IndividualBuilder
-from music import *
 
 def plot_genes_matrix(matrix, x, title, labels=None):
     edge = max(max(matrix[0]), max(matrix[0]))
     edge = edge + 0.5 * edge
     edge = [edge]*len(x)
     matrix.append(edge)
-    labels.append('edge')
+    if labels:
+        labels.append('edge')
+
 
     sum = []
     for i in range(len(x)):
         sum.append(matrix[0][i]+matrix[1][i])
     matrix.append(sum)
-    labels.append('i1+i2')
+    if labels:
+        labels.append('i1+i2')
 
     for i in range(len(matrix)):
-        if not labels is None:
-            plt.plot(x, matrix[i], label = labels[i])
+        if labels:
+            plt.plot(x, matrix[i], label=labels[i])
         else:
             plt.plot(x, matrix[i])
 
@@ -37,10 +42,4 @@ def plot_first_gene_from_individuals(i1, i2, i3):
 
 def plot_genes(i):
     plot_genes_matrix(i.note_genes, i.possible_notes, 'Note genes')
-    # plot_genes_matrix(i.duration_genes, i.possible_durations, 'Duration genes')
-    # plot_genes_matrix(i.dynamic_genes, i.possible_dynamic, 'Dynamics genes')
-
-i1 = IndividualBuilder().from_chance(C5, MAJOR_SCALE, 8)
-i2 = IndividualBuilder().from_chance(C5, MAJOR_SCALE, 8)
-i3 = IndividualBuilder().from_individuals(i1, i2)
-plot_first_gene_from_individuals(i1, i2, i3)
+    plot_genes_matrix(i.duration_genes, i.possible_durations, 'Duration genes')
